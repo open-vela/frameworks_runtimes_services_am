b03af6c3f228bcc6b41a2e729d92035dbee0c897
pengchen1 <pengchen1@xiaomi.com>
2023-06-08 10:23:30 +0800

    add am aidl interface
    define IActivityManager interface,IApplicationThread interface.
    
    Signed-off-by: pengchen1 <pengchen1@xiaomi.com>

---
+++ include/app/Intent.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <binder/Parcel.h>
+#include <binder/PersistableBundle.h>
+#include <binder/Status.h>
+
+#include <string>
+
+namespace os {
+namespace app {
+
+class Intent : public android::Parcelable {
+public:
+    std::string mTarget;
+    std::string mAction;
+    std::string mData;
+    uint32_t mFlag;
+    /** PersistableBundle,a mapping from String values to various types */
+    android::os::PersistableBundle mExtra;
+
+    enum {
+        NO_FLAG = 0,
+        FLAG_ACTIVITY_NEW_TASK = 1,
+        FLAG_ACTIVITY_SINGLE_TOP = 2,
+        FLAG_ACTIVITY_CLEAR_TOP = 4,
+        FLAG_ACTIVITY_CLEAR_TASK = 8,
+    };
+
+    Intent() : mFlag(NO_FLAG){};
+    ~Intent() = default;
+
+    Intent(const std::string& target) : mTarget(target) {}
+    void setTarget(const std::string& target);
+    void setAction(const std::string& action);
+    void setData(const std::string& data);
+    void setFlag(const int32_t flag);
+    void setBundle(const android::os::PersistableBundle& extra);
+
+    android::status_t readFromParcel(const android::Parcel* parcel) final;
+    android::status_t writeToParcel(android::Parcel* parcel) const final;
+
+public:
+    /****************** action definition *****************/
+    static const std::string ACTION_HOME;
+    /******************************************************/
+}; // class Intent
+
+} // namespace app
+} // namespace os
