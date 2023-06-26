aaf3e2d445a4bd2afc6bc8155531aa0cd6204495
pengchen1 <pengchen1@xiaomi.com>
2023-06-26 22:05:29 +0800

    add Application runtime
    Define the basic framework of the application
    
    Signed-off-by: pengchen1 <pengchen1@xiaomi.com>

---
+++ app/Application.cpp
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "app/Application.h"
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <utils/Log.h>
+
+#include "app/ActivityManager.h"
+
+namespace os {
+namespace app {
+
+Application::Application() {
+    mUid = getuid();
+    mPid = getpid();
+}
+
+Application::~Application() {}
+
+string Application::getPackageName() const {
+    return mPackageName;
+}
+
+void Application::registerActivity(const string& name, const CreateActivityFunc& createFunc) {
+    mActivityMap.emplace(name, createFunc);
+    ALOGD("Application registerActivity:%s", name.c_str());
+}
+
+std::shared_ptr<Activity> Application::createActivity(const string& name) {
+    auto it = mActivityMap.find(name);
+    if (it != mActivityMap.end()) {
+        return std::shared_ptr<Activity>(it->second());
+    }
+    ALOGE("Application createActivity failed:%s", name.c_str());
+    return nullptr;
+}
+
+void Application::registerService(const string& name, const CreateServiceFunc& createFunc) {
+    mServiceMap.emplace(name, createFunc);
+    ALOGD("Application registerService:%s", name.c_str());
+}
+
+std::shared_ptr<Service> Application::createService(const string& name) {
+    auto it = mServiceMap.find(name);
+    if (it != mServiceMap.end()) {
+        return std::shared_ptr<Service>(it->second());
+    }
+    ALOGE("Application createService failed:%s", name.c_str());
+    return nullptr;
+}
+
+void Application::addActivity(const sp<IBinder>& token, const std::shared_ptr<Activity>& activity) {
+    mExistActivities.insert({token, activity});
+}
+
+std::shared_ptr<Activity> Application::findActivity(const sp<IBinder>& token) {
+    auto it = mExistActivities.find(token);
+    if (it != mExistActivities.end()) {
+        return it->second;
+    } else {
+        return nullptr;
+    }
+}
+
+void Application::deleteActivity(const sp<IBinder>& token) {
+    auto it = mExistActivities.find(token);
+    if (it != mExistActivities.end()) {
+        mExistActivities.erase(it);
+    }
+}
+
+} // namespace app
+} // namespace os
---
+++ app/ContextWrapper.cpp
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "app/Context.h"
+
+namespace os {
+namespace app {
+
+std::shared_ptr<Context> ContextWrapper::getContext() {
+    return mBase;
+}
+
+void ContextWrapper::attachBaseContext(std::shared_ptr<Context> base) {
+    if (mBase != nullptr) {
+        ALOGE("Base context already set");
+    }
+    mBase = base;
+}
+
+std::string ContextWrapper::getPackageName() {
+    return mBase->getPackageName();
+}
+
+void ContextWrapper::startActivity(const Intent& intent) {
+    return mBase->startActivity(intent);
+}
+
+void ContextWrapper::startActivityForResult(const Intent& intent, int requestCode) {
+    return mBase->startActivityForResult(intent, requestCode);
+}
+
+void ContextWrapper::startService(const Intent& intent) {
+    return mBase->startService(intent);
+}
+
+void ContextWrapper::reportActivityStatus(const int status) {
+    return mBase->reportActivityStatus(status);
+}
+
+void ContextWrapper::setIntent(const Intent& intent) {
+    return mBase->setIntent(intent);
+}
+
+const Intent& ContextWrapper::getIntent() {
+    return mBase->getIntent();
+}
+
+} // namespace app
+} // namespace os
