6f2dd43158221e978c1b407c7e25c9f458cde944
pengchen1 <pengchen1@xiaomi.com>
2023-06-26 22:20:52 +0800

    add base structure about ActivityRecord
    Change-Id: I248d37e19209b3ee71c2719169e9d6522da74130

---
+++ server/AppRecord.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "AppRecord.h"
+
+namespace os {
+namespace am {
+
+using namespace std;
+
+ActivityHandler AppRecord::checkActivity(const std::string& activityName) {
+    const int size = mExistActivity.size();
+    for (int i = 0; i < size; ++i) {
+        if (mExistActivity[i].lock()->mActivityName == activityName) {
+            return mExistActivity[i].lock();
+        }
+    }
+    return nullptr;
+}
+
+const shared_ptr<AppRecord> AppInfoList::findAppInfo(const int pid) {
+    const int size = mAppList.size();
+    for (int i = 0; i < size; ++i) {
+        if (mAppList[i]->mPid == pid) {
+            return mAppList[i];
+        }
+    }
+    return nullptr;
+}
+
+const shared_ptr<AppRecord> AppInfoList::findAppInfo(const string& packageName) {
+    const int size = mAppList.size();
+    for (int i = 0; i < size; ++i) {
+        if (mAppList[i]->mPackageName == packageName) {
+            return mAppList[i];
+        }
+    }
+    return nullptr;
+}
+
+bool AppInfoList::addAppInfo(const shared_ptr<AppRecord>& appInfo) {
+    if (nullptr == findAppInfo(appInfo->mPid).get()) {
+        mAppList.emplace_back(appInfo);
+        return true;
+    }
+    return false;
+}
+
+void AppInfoList::deleteAppInfo(const int pid) {
+    const int size = mAppList.size();
+    for (int i = 0; i < size; ++i) {
+        if (mAppList[i]->mPid == pid) {
+            mAppList[i] = mAppList[size - 1];
+            mAppList.pop_back();
+            break;
+        }
+    }
+}
+
+void AppInfoList::deleteAppInfo(const string& packageName) {
+    const int size = mAppList.size();
+    for (int i = 0; i < size; ++i) {
+        if (mAppList[i]->mPackageName == packageName) {
+            mAppList[i] = mAppList[size - 1];
+            mAppList.pop_back();
+            break;
+        }
+    }
+}
+
+} // namespace am
+} // namespace os
---
+++ server/AppRecord.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "ActivityRecord.h"
+#include "os/app/IApplicationThread.h"
+
+namespace os {
+namespace am {
+
+using android::sp;
+using os::app::IApplicationThread;
+
+struct AppRecord {
+    sp<IApplicationThread> mAppThread;
+    std::string mPackageName;
+    int mPid;
+    int mUid;
+    std::vector<std::weak_ptr<ActivityRecord>> mExistActivity;
+
+    AppRecord(sp<IApplicationThread> app, std::string packageName, int pid, int uid)
+          : mAppThread(app), mPackageName(packageName), mPid(pid), mUid(uid) {}
+
+    ActivityHandler checkActivity(const std::string& activityName);
+};
+
+class AppInfoList {
+public:
+    const std::shared_ptr<AppRecord> findAppInfo(const int pid);
+    const std::shared_ptr<AppRecord> findAppInfo(const std::string& packageName);
+    bool addAppInfo(const std::shared_ptr<AppRecord>& appInfo);
+    void deleteAppInfo(const int pid);
+    void deleteAppInfo(const std::string& packageName);
+
+private:
+    std::vector<std::shared_ptr<AppRecord>> mAppList;
+};
+
+} // namespace am
+} // namespace os
