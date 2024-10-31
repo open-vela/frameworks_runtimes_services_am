ba7bc4e9e6e49539f35c8801a3db9e2668b4b9e8
qinliansong <qinliansong@xiaomi.com>
2024-10-31 13:32:07 +0800

    fix xiaoaivoice move to background fail
    because of xiaoaivoice becoming hometask, it cannot move to background
    test by specific product smartspeaker
    Signed-off-by: liaozhiwei <liaozhiwei@xiaomi.com>

--- server/TaskStackManager.cpp
+++ server/TaskStackManager.cpp
@@ -66,17 +66,26 @@ bool TaskStackManager::moveTaskToBackground(const ActivityStackHandler& targetSt
     }
 
     // If it's above HomeTask, move it below HomeTask, otherwise no need to move it
-    auto hometask_index = std::find(mAllTasks.begin(), mAllTasks.end(), mHomeTask);
-    if (hometask_index == mAllTasks.end()) {
-        mAllTasks.remove(targetStack);
-        mAllTasks.push_back(targetStack);
-    } else {
-        for (auto iter = mAllTasks.begin(); iter != hometask_index; ++iter) {
-            if (*iter == targetStack) {
-                mAllTasks.erase(iter);
-                mAllTasks.insert(++hometask_index, targetStack);
+    for (auto iter = mAllTasks.begin(); iter != mAllTasks.end();) {
+        if (*iter == targetStack) {
+            isBeforeHomeTask = true;
+            auto tmp = iter;
+            ++iter;
+            mAllTasks.erase(tmp);
+        } else {
+            if (mHomeTask == nullptr) {
+                if (isBeforeHomeTask) {
+                    mAllTasks.push_back(targetStack);
+                    break;
+                }
+            }
+            if (*iter == mHomeTask) {
+                if (isBeforeHomeTask) {
+                    mAllTasks.insert(++iter, targetStack);
+                }
                 break;
             }
+            ++iter;
         }
     }
 
@@ -85,7 +94,7 @@ bool TaskStackManager::moveTaskToBackground(const ActivityStackHandler& targetSt
         auto topActivity = targetStack->getTopActivity();
         topActivity->lifecycleTransition(ActivityRecord::PAUSED);
         targetStack->setForeground(false);
-        mAllTasks.pop_front();
+        // mAllTasks.pop_front();
         activeTask = getActiveTask();
         if (activeTask) {
             auto nextActivity = activeTask->getTopActivity();
@@ -98,25 +107,7 @@ bool TaskStackManager::moveTaskToBackground(const ActivityStackHandler& targetSt
         }
     }
 
-    // If it's above HomeTask, move it below HomeTask, otherwise no need to move it
-    for (auto iter = mAllTasks.begin(); iter != mAllTasks.end();) {
-        if (*iter == targetStack) {
-            isBeforeHomeTask = true;
-            auto tmp = iter;
-            ++iter;
-            mAllTasks.erase(tmp);
-        } else {
-            if (*iter == mHomeTask) {
-                if (isBeforeHomeTask) {
-                    mAllTasks.insert(++iter, targetStack);
-                }
-                return true;
-            }
-            ++iter;
-        }
-    }
-
-    return false;
+    return isBeforeHomeTask;
 }
 
 void TaskStackManager::pushNewActivity(const ActivityStackHandler& targetStack,
@@ -128,8 +119,6 @@ void TaskStackManager::pushNewActivity(const ActivityStackHandler& targetStack,
     if (activeTask) {
         lastTopActivity = activeTask->getTopActivity();
         lastTopActivity->lifecycleTransition(ActivityRecord::PAUSED);
-    } else {
-        mHomeTask = targetStack;
     }
 
     if (startFlag & Intent::FLAG_ACTIVITY_CLEAR_TASK) {
