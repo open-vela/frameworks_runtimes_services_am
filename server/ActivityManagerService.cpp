bb10a89282ad686e5e61e45e23fbd598416c02ee
lijianbin6 <lijianbin6@xiaomi.com>
2025-09-29 10:29:47 +0800

    Ams systemready: add bootcomplete property.
    Signed-off-by: lijianbin6 <lijianbin6@xiaomi.com>

--- server/ActivityManagerService.cpp
+++ server/ActivityManagerService.cpp
@@ -1048,6 +1048,8 @@ void ActivityManagerInner::systemReady() {
     broadcastIntent(intent, IntentAction::COMP_TYPE_SERVICE);
     broadcastIntent(intent, IntentAction::COMP_TYPE_ACTIVITY);
 
+    property_set_bool("bootcomplete", true);
+
     AM_PROFILER_END();
     return;
 }
